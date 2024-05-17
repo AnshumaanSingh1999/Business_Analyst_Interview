9c560bb9c9dae5505a22a55d128a4e10f78efd0d
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2024-05-17 21:42:56 +0530

    Update sql_Q2_2.sql

--- sql_Q2_2.sql
+++ sql_Q2_2.sql
@@ -1,1 +1,1 @@
-SELECT eid,sal FROM pd WHERE dep='Eng' ORDER BY sal DESC LIMIT 1 OFFSET 1;
+SELECT * FROM (SELECT *, (Rank() OVER (PARTITION BY dep ORDER BY sal DESC)) AS Ranking FROM empl_max_dep_sal) AS Ranked_Table WHERE Ranking=2 AND dep="Sales";
