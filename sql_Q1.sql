68cb6326870e6c9c29813861572086c4394855af
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2024-05-17 20:56:56 +0530

    Update sql_Q1.sql

--- sql_Q1.sql
+++ sql_Q1.sql
@@ -1,1 +1,1 @@
-SELECT name FROM (SELECT *,(Rank() over (PARTITION BY dep ORDER BY sal DESC)) AS Ranking FROM empl_max_dep_sal) AS Artificial_Table WHERE Ranking=1;
+SELECT name FROM (SELECT *,(Rank() over (PARTITION BY dep ORDER BY sal DESC)) AS Ranking FROM empl_max_dep_sal) AS Ranked_Table WHERE Ranking=1;
